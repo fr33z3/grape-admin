c28532449df6402ec59f34d3395038633f3ebc21
Sergei Gribovskii <sergei.gribovskii@dojomadness.com>
2018-02-25 01:17:04 +0100

    initial commit

---
+++ lib/grape/admin.rb
@@ -0,0 +1,7 @@
+require "grape/admin/version"
+
+module Grape
+  module Admin
+    # Your code goes here...
+  end
+end
---
+++ spec/grape/admin_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Grape::Admin do
+  it "has a version number" do
+    expect(Grape::Admin::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
